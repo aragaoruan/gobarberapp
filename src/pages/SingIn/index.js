d542548428c42fdde2f91657ee991d42ce6f0e4e
Ruan Aragao <aragao.ruan@gmail.com>
2019-12-02 16:16:04 -0300

    Backgroud configurado.

--- src/pages/SingIn/index.js
+++ src/pages/SingIn/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import Background from '~/components/Background';
+
 // import { Container } from './styles';
 
 export default function SingIn() {
-  return <Text>Sing In</Text>;
+  return (
+    <Background>
+      <Text>Sing In</Text>
+    </Background>
+  );
 }
