71ec30e44e7f9a8d344c403924b2360911abf625
Ruan Aragao <aragao.ruan@gmail.com>
2019-12-10 15:35:13 -0300

    configurando reactotron

---
+++ src/config/ReactotronConfig.js
@@ -0,0 +1,17 @@
+import Reactotron from 'reactotron-react-native';
+import { reactotronRedux } from 'reactotron-redux';
+import reactotronSaga from 'reactotron-redux-saga';
+
+if (__DEV__) {
+  const host = { host: '192.168.43.7' };// YOUR_APP_ADRESS
+
+  const tron = Reactotron.configure(host)
+    .useReactNative()
+    .use(reactotronRedux())
+    .use(reactotronSaga())
+    .connect();
+
+  tron.clear();
+
+  console.tron = tron;
+}
