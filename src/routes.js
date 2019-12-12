a2b952ca2053a1faf84ff8b44aa816866c838570
Ruan Aragao <aragao.ruan@gmail.com>
2019-12-12 15:10:37 -0300

    rotas inicial

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,29 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
+/**
+ * NAO LOGADO
+ */
 import SingIn from '~/pages/SingIn';
 import SingUp from '~/pages/SingUp';
 
-export default createAppContainer(
-  createSwitchNavigator({
-    SingIn,
-    SingUp,
-  })
-);
+/**
+ * LOGADO
+ */
+import Dashboard from '~/pages/Dashboard';
+
+export default (signedIn = false) =>
+  createAppContainer(
+    createSwitchNavigator(
+      {
+        Sign: createSwitchNavigator({
+          SingIn,
+          SingUp,
+        }),
+        App: createBottomTabNavigator({
+          Dashboard,
+        }),
+      },
+      { initialRouteName: signedIn ? 'App' : 'Sign' }
+    )
+  );
