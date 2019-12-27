1581a2bdb520c7c29b7febc3d11b39ac802d7256
Ruan Aragao <aragao.ruan@gmail.com>
2019-12-27 16:30:14 -0300

    listando agendamentos

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -6,6 +6,7 @@ import AsyncStorage from '@react-native-community/async-storage';
 
 if (__DEV__) {
   const host = { host: '192.168.43.7' }; // YOUR_APP_ADRESS
+  // const host = { host: '10.230.30.69' }; // YOUR_APP_ADRESS
 
   const tron = Reactotron.setAsyncStorageHandler(AsyncStorage)
     .configure(host)
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
+  // baseURL: 'http://10.230.30.69:3333',
   baseURL: 'http://192.168.43.7:3333',
 });
 
